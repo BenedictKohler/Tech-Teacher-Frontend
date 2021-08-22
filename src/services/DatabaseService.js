b1f6f0c9ae47a141a8d8553c83bb5d146edefa10
Benedict Kohler <bkohler@muhlenberg.edu>
2021-08-22 18:29:39 +0100

    Fixed url to backend

--- src/services/DatabaseService.js
+++ src/services/DatabaseService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/'
+    baseURL: 'https://tech-teacher-backend.herokuapp.com/'
 });
 
 class DatabaseService {
